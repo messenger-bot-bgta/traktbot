f6615ce3a6482e170786498ded8d4e5815d09ac3
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2017-07-15 11:04:03 -0500

    User

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,5 +1,15 @@
 include Facebook::Messenger
 
 Bot.on :message do |message|
+
+  user = User.first_or_create(messenger_id: message.sender['id'])
+
   message.reply(text: 'Hello, human!')
+end
+
+Bot.on :postback do |postback|
+
+  user = User.first_or_create(messenger_id: postback.sender['id'])
+
+  postback.reply(text: 'Hello, human!')
 end
